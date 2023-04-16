23e83cec99d917723722b375e21b1341afabed5e
Romain Lancelot <rlancelot@myges.fr>
2023-04-16 15:19:58 +0200

    initial commit

---
+++ script/beer.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById("#beer").style.display = "none";
+        document.getElementById("#main").style.display = "block";
+    }, 3000);
+}
